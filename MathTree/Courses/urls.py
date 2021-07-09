587110889173aea61c45d0a8f5bb7c121897b1e7
Fafookalfon <raphaelkalfon9@gmail.com>
2021-07-10 00:28:35 +0200

    Added the tree_view.html and general tree view

--- MathTree/Courses/urls.py
+++ MathTree/Courses/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', views.courses.as_view(), name = "courses"),
     path('<str:course_name>/', views.course_view, name="course_view"),
     path('<str:course_name>/<int:chapter_index>/', views.chapter_view, name= "chapter_view"),
-    path('<str:course_name>/<int:chapter_index>/<int:page_index>', views.page_view, name="page_view")
+    path('<str:course_name>/<int:chapter_index>/<int:page_index>', views.page_view, name="page_view"),
+    path('general_tree_view', views.general_tree_view, name="general_tree_view")
 ]
--- MathTree/Courses/views.py
+++ MathTree/Courses/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic.list import ListView
-from .models import Course, Chapter, ChapterPage
+from .models import Course, Chapter, ChapterPage, Prerequisite
+import json
 
 class courses(ListView) : 
 
@@ -9,7 +10,6 @@ class courses(ListView) :
 
     def get_queryset(self) : 
         return Course.objects.all()
-
 
 def course_view(request, course_name) :
 
@@ -25,3 +25,41 @@ def page_view(request, course_name, chapter_index, page_index) :
 
     page = ChapterPage.objects.filter(chapter__course__name=course_name, chapter__index=chapter_index, index=page_index).first()
     return render(request, 'Courses/page_view.html', {"page" : page})
+
+### This is dedicated to generating all the json necessary for the tree rendering. ###
+
+def prerequisite_to_json(Prerequisite) :
+
+    result = {}
+    result.update({"id" : f"{Prerequisite.prerequisite.name} to {Prerequisite.target.name}" })
+    result.update({"source" : Prerequisite.prerequisite.name})
+    result.update({"target" : Prerequisite.target.name})
+
+    return result
+
+def course_to_json(course) : 
+
+    result = {}
+    result.update({"id" : course.name})
+    result.update({"label" : course.name})
+    result.update({"x" : course.x_position})
+    result.update({"y" : course.y_position})
+    result.update({"size" : 3})
+
+    return result
+
+def generate_tree_data_as_json() :
+
+    result = { "nodes" : [], "edges" : []  }
+    
+    for course in Course.objects.all() :
+        result["nodes"].append(course_to_json(course))
+    
+    for prerequisite in Prerequisite.objects.all() :
+        result["edges"].append(prerequisite_to_json(prerequisite))
+    
+    return json.dumps(result)
+
+def general_tree_view(request) : 
+    return render(request, "Courses/tree_view.html", context = {"data" : generate_tree_data_as_json()})
+
