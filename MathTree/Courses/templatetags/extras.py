afc201a36031d5b7015b9e42044a6dd1ab5be62a
Fafookalfon <raphaelkalfon9@gmail.com>
2021-07-10 23:29:32 +0200

    Added differentiated view for different fields of a chapter_page

--- MathTree/Courses/templatetags/extras.py
+++ MathTree/Courses/templatetags/extras.py
@@ -1,5 +1,6 @@
 from django.template import Library
 import ast
+from ..models import *
 register = Library()
 
 @register.filter
@@ -11,6 +12,25 @@ def get_identifier(l):
     return l[0]
 
 @register.filter
-def get_content(l) :
+def get_zero_field(l) :
+    return l[0]
+
+@register.filter
+def get_first_field(l) :
     return l[1]
 
+@register.filter
+def get_second_field(l) :
+    return l[2]
+
+@register.filter
+def get_third_field(l) :
+    return l[3]
+
+@register.filter
+def get_fourth_field(l) :
+    return l[4]
+
+@register.filter
+def get_associated_exercise_fields(ex_name) : 
+    return [Exercise.objects.filter(name=ex_name).first().questions, Exercise.objects.filter(name=ex_name).first().hints, Exercise.objects.filter(name=ex_name).first().solutions]
