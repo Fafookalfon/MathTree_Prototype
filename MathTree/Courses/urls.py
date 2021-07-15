68774f71f0e7cb5ec302299b12717c00457814d9
Fafookalfon <raphaelkalfon9@gmail.com>
2021-07-15 22:23:32 +0200

    Added the embryo of a comment system

--- MathTree/Courses/urls.py
+++ MathTree/Courses/urls.py
@@ -7,6 +7,9 @@ urlpatterns = [
     path('<str:course_name>/', views.course_view, name="course_view"),
     path('<str:course_name>/<int:chapter_index>/', views.chapter_view, name= "chapter_view"),
     path('<str:course_name>/<int:chapter_index>/<int:page_index>', views.page_view, name="page_view"),
+    path('<str:course_name>/<int:chapter_index>/<int:page_index>/comment', views.page_comment_view, name="page_comment_view"),
     path('general_tree_view', views.general_tree_view, name="general_tree_view"),
-    path('<str:course_name>/exercises_view', views.exercises_view, name="exercises_view")
+    path('<str:course_name>/exercises_view', views.exercises_view, name="exercises_view"),
+    path('<str:course_name>/<int:chapter_index>/<int:page_index>/send_comment/<int:solution>', views.send_comment, name="send_comment")
+    
 ]
--- MathTree/Courses/views.py
+++ MathTree/Courses/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.views.generic.list import ListView
 from .models import Course, Chapter, ChapterPage, Prerequisite
 import json
+from random import randint
 
 class courses(ListView) : 
 
@@ -30,6 +31,32 @@ def exercises_view(request, course_name) :
 
     course = Course.objects.filter(name=course_name).first()
     return render(request, 'Courses/exercises_view.html', {"course" : course})
+
+def page_comment_view(request, course_name, chapter_index, page_index ) :
+    page = ChapterPage.objects.filter(chapter__course__name=course_name, chapter__index=chapter_index, index=page_index).first()
+    captcha = enigma()
+    return render(request, 'Courses/page_comment.html', {"page" : page, "grand_nombre_lettres" : captcha.get("grand_nombre_lettres"), "petit_nombre_lettres" :  captcha.get("petit_nombre_lettres"), "operation" :  captcha.get("operation"), "solution" :  captcha.get("solution")})
+
+def send_comment(request, course_name, chapter_index, page_index, solution) :
+
+    course = Course.objects.filter(name=course_name).first()
+    chapter = course.chapter_set.filter(index=chapter_index).first()
+    page = chapter.chapterpage_set.filter(index=page_index).first()
+
+    if not request.method == 'POST' :
+        print("NOT POST")
+        return render(request, "Courses/rejected_comment.html", {"page" : page})
+    
+    if str(request.POST.get("solution")) != str(solution) :
+        print("NOT SOlUTION")
+        return render(request, "Courses/rejected_comment.html", {"page" : page})
+
+    '''
+    new_comment = Comment(page = page, name = request.POST.get("name"), content= request.POST.get("content"))
+    new_comment.save()
+    '''
+    
+    return render(request, "Courses/accepted_comment.html", {"page" : page}) 
 
 ### This is dedicated to generating all the json necessary for the tree rendering. ###
 
@@ -68,3 +95,23 @@ def generate_tree_data_as_json() :
 def general_tree_view(request) : 
     return render(request, "Courses/tree_view.html", context = {"data" : generate_tree_data_as_json()})
 
+### Those functions are pure utilities. ###
+
+def enigma() :
+
+    operations = ["plus", "moins"]
+    nombres = ['zéro', 'un', 'deux', 'trois', 'quatre', 'cinq', 'six', 'sept', 'huit', 'neuf', 'dix', 'onze', 'douze', 'treize', 'quatorze', 'quinze', 'seize', 'dix-sept', 'dix-huit', 'dix-neuf', 'vingt', 'vingt et un', 'vingt-deux', 'vingt-trois', 'vingt-quatre', 'vingt-cinq', 'vingt-six', 'vingt-sept', 'vingt-huit', 'vingt-neuf', 'trente', 'trente et un', 'trente-deux', 'trente-trois', 'trente-quatre', 'trente-cinq', 'trente-six', 'trente-sept', 'trente-huit', 'trente-neuf', 'quarante', 'quarante et un', 'quarante-deux', 'quarante-trois', 'quarante-quatre', 'quarante-cinq', 'quarante-six', 'quarante-sept', 'quarante-huit', 'quarante-neuf', 'cinquante', 'cinquante et un', 'cinquante-deux', 'cinquante-trois', 'cinquante-quatre', 'cinquante-cinq', 'cinquante-six', 'cinquante-sept', 'cinquante-huit', 'cinquante-neuf', 'soixante', 'soixante et un', 'soixante-deux', 'soixante-trois', 'soixante-quatre', 'soixante-cinq', 'soixante-six', 'soixante-sept', 'soixante-huit', 'soixante-neuf', 'soixante-dix', 'soixante et onze', 'soixante-douze', 'soixante-treize', 'soixante-quatorze', 'soixante-quinze', 'soixante-seize', 'soixante-dix-sept', 'soixante-dix-huit', 'soixante-dix-neuf', 'quatre-vingts', 'quatre-vingt-un', 'quatre-vingt-deux', 'quatre-vingt-trois', 'quatre-vingt-quatre', 'quatre-vingt-cinq', 'quatre-vingt-six', 'quatre-vingt-sept', 'quatre-vingt-huit', 'quatre-vingt-neuf', 'quatre-vingt-dix', 'quatre-vingt-onze', 'quatre-vingt-douze', 'quatre-vingt-treize', 'quatre-vingt-quatorze', 'quatre-vingt-quinze', 'quatre-vingt-seize', 'quatre-vingt-dix-sept', 'quatre-vingt-dix-huit', 'quatre-vingt-dix-neuf', 'cent']
+    
+    grand_nombre_int = randint(40,60)
+    petit_nombre_int = randint(0,20)
+    grand_nombre_lettres = nombres[grand_nombre_int]
+    petit_nombre_lettres = nombres[petit_nombre_int]
+    operation = operations[randint(0,1)]
+    solution = grand_nombre_int
+
+    if operation == "plus" :
+        solution += petit_nombre_int
+    
+    else : solution -= petit_nombre_int
+
+    return {"grand_nombre_lettres" : grand_nombre_lettres, "petit_nombre_lettres" : petit_nombre_lettres, "operation" : operation, "solution" : str(solution)}
